425b5442c5ebf536a919d483fc202ebc49515f5e
Maraba23 <78489243+Maraba23@users.noreply.github.com>
2023-05-15 22:00:27 -0300

    Hot Roll de camarao

--- application/src/main/java/com/delivery/application/viagem/ViagemController.java
+++ application/src/main/java/com/delivery/application/viagem/ViagemController.java
@@ -12,27 +12,24 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/viagem")
 public class ViagemController {
-    
+
     @Autowired
     private ViagemService viagemService;
 
     @GetMapping
-    public ResponseEntity<List<ViagemDTO>> getAllViagens() {
-        List<ViagemDTO> viagens = viagemService.findAll();
-        return new ResponseEntity<>(viagens, HttpStatus.OK);
+    public List<Viagem> findAll() {
+        return viagemService.findAll();
     }
 
     @GetMapping("/{identifier}")
-    public ResponseEntity<ViagemDTO> findByIdentifier(@PathVariable String identifier) {
-        ViagemDTO viagem = viagemService.findByIdentifier(identifier);
-        return new ResponseEntity<>(viagem, HttpStatus.OK);
+    public Viagem findByIdentifier(@PathVariable String identifier) {
+        return viagemService.findByIdentifier(identifier);
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public ResponseEntity<ViagemDTO> save(@RequestBody @Valid ViagemDTO viagemDTO) {
-        ViagemDTO savedViagem = viagemService.save(viagemDTO);
-        return new ResponseEntity<>(savedViagem, HttpStatus.CREATED);
+    public Viagem save(@RequestBody @Valid Viagem viagem) {
+        return viagemService.save(viagem);
     }
 
     @DeleteMapping("/{identifier}")
@@ -42,14 +39,7 @@ public class ViagemController {
     }
 
     @PutMapping
-    public ResponseEntity<ViagemDTO> update(@RequestBody @Valid ViagemDTO viagemDTO) {
-        ViagemDTO updatedViagem = viagemService.update(viagemDTO);
-        return new ResponseEntity<>(updatedViagem, HttpStatus.OK);
-    }
-
-    @PutMapping("/{identifier}/status/")
-    public ResponseEntity<ViagemDTO> updateStatusByIdentifier(@PathVariable String identifier) {
-        ViagemDTO updatedViagem = viagemService.updateStatusByIdentifier(identifier);
-        return new ResponseEntity<>(updatedViagem, HttpStatus.OK);
+    public Viagem update(@RequestBody @Valid Viagem viagem) {
+        return viagemService.update(viagem);
     }
 }
