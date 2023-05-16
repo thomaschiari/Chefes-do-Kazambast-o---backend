3da097317b02fbcc78e2e30a16595b58540af49c
thomaschiari <thomaschiari@gmail.com>
2023-05-16 09:05:54 -0300

    FUNCIONOU! VITOR PEREIRA NÃO VENCEU!

--- application/src/main/java/com/delivery/application/viagem/ViagemService.java
+++ application/src/main/java/com/delivery/application/viagem/ViagemService.java
@@ -2,8 +2,12 @@ package com.delivery.application.viagem;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
 import java.util.List;
 import java.util.UUID;
+
 @Service
 public class ViagemService {
     @Autowired
@@ -16,6 +20,8 @@ public class ViagemService {
     }
     public Viagem save(Viagem viajem) {
         viajem.setIdentifier(UUID.randomUUID().toString());
+        Integer idMotorista = obterIdMotoristaDisponivel();
+        viajem.setIdMotorista(idMotorista);
         return viagemRepository.save(viajem);
     }
     @Transactional
@@ -32,4 +38,24 @@ public class ViagemService {
         viagemRepository.confirmByIdentifier(identifier);
     }
 
+    private RestTemplate restTemplate = new RestTemplate();
+
+    private Integer obterIdMotoristaDisponivel() {
+        String url = "http://192.168.10.174:8080/entregador";
+    
+        // Faz a requisição GET para a API externa e obtém a resposta como HashMap
+        HashMap<String, Object>[] response = restTemplate.getForObject(url, HashMap[].class);
+    
+        if (response != null && response.length > 0) {
+            for (HashMap<String, Object> driver : response) {
+                String statusOcupacao = (String) driver.get("status_ocupacao");
+    
+                if ("DISPONIVEL".equals(statusOcupacao)) {
+                    return (Integer) driver.get("id");
+                }
+            }
+        }
+    
+        return null; // Retorna null caso não haja motoristas disponíveis
+    }
 }
